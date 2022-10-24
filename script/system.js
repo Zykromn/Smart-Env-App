8c926ce6b2c2b72e6384ef37329cbb789ec905df
Zykromn_201 <108986336+Zykromn@users.noreply.github.com>
2022-10-24 12:04:42 +0600

    Add files via upload

--- script/system.js
+++ script/system.js
@@ -2,10 +2,11 @@
 let SYS_HTTPRequestCount = 0;
 let BMM_IconClose = $('.BMM-BurgerMenuClose');
 let BMH_IconClose = $('.BMH-BurgerMenuClose');
+let BMC_status = 0;
 let BM_Icon = $('.Header-BurgerMenuIcon');
 let MD_Content = $(".MD-Content");
+let MM_Content = $(".MM-Content");
 let MI_Content = $(".MI-Content");
-let MF_Content = $(".MF-Content");
 let APP_SwipeStart = 0;
 let APP_SwipeEnd = 0;
 
@@ -13,17 +14,21 @@ const SES_UOFNameList= {
     "d0000": "Distance",
     "d0001": "Humidity",
     "d0002": "Temperature",
-    "d0003": "Gas leak",
-    "d0004": "Water leak",
-    "m0000": "Lamp"
+    "i0000": "Water leak",
+    "i0001": "Gas leak",
+    "m0000": "Lamp",
+    "m0001": "Output 1",
+    "m0002": "Output 2",
+    "m0003" : "Servo door"
 }
 const SED_UOFIdList = {
     "d0000": "cm.",
     "d0001": "%",
     "d0002": "C."
 }
-const SEF_UOFValList = {
-    "d0004": {1: "LEAK DETECTED", 0: "Normal"}
+const SEI_UOFValList = {
+    "i0000": {0: "Normal", 1: "Leak detected"},
+    "i0001": {0: "Normal", 1: "Leak detected"}
 }
 
 
@@ -62,7 +67,7 @@ function HTTP_Request() {
 
 
         if (SYS_HTTPRequestCount !== 0) {
-            APP_CookieJson = SYS_MIContent(APP_CookieJson);
+            APP_CookieJson = SYS_MMContent(APP_CookieJson);
         }
 
         let xhr = new XMLHttpRequest();
@@ -90,8 +95,8 @@ function HTTP_Working(HTTP_Response) {
         window.location.href = 'server-error.html';
     } else {
         MD_Content.html("");
-        MF_Content.html("");
-        if (SYS_HTTPRequestCount === 1) {MI_Content.html("")}
+        MI_Content.html("");
+        if (SYS_HTTPRequestCount === 1) {MM_Content.html("")}
 
         for (let ITEM_JSONResponse in JSON_ResponseINFOOBJ) {
             if (ITEM_JSONResponse in SED_UOFIdList) {
@@ -102,21 +107,25 @@ function HTTP_Working(HTTP_Response) {
                     </div>
                 `;
                 MD_Content.html(MD_Content.html() + HTML_InfoPart);
-            } else if (ITEM_JSONResponse in SEF_UOFValList) {
+            } else if (ITEM_JSONResponse in SEI_UOFValList) {
                 function SES_UOFValUi() {
                     let HTML_DivInfoPart;
 
                     if (JSON_ResponseINFOOBJ[ITEM_JSONResponse]){
                         HTML_DivInfoPart = `
-                            <div class="MF-ItemValue" style="margin-right: 2vw; color: #ff0000; font-weight: 900;">
-                                ${SEF_UOFValList[ITEM_JSONResponse][JSON_ResponseINFOOBJ[ITEM_JSONResponse]]}
+                            <div class="MI-ItemValue" style="margin-right: 2vw; color: #ff0000; font-weight: 900;">        
+                                <!--  ${JSON_ResponseINFOOBJ[ITEM_JSONResponse]} -->                     
+                                ${SEI_UOFValList[ITEM_JSONResponse][JSON_ResponseINFOOBJ[ITEM_JSONResponse]]} 
+                                <!-- ${SEI_UOFValList[ITEM_JSONResponse]} -->
                             </div>
 
                         `;
                     } else {
                         HTML_DivInfoPart = `
-                            <div class="MF-ItemValue" style="margin-right: 2vw"> 
-                                ${SEF_UOFValList[ITEM_JSONResponse][JSON_ResponseINFOOBJ[ITEM_JSONResponse]]}
+                            <div class="MI-ItemValue" style="margin-right: 2vw"> 
+                                <!-- ${JSON_ResponseINFOOBJ[ITEM_JSONResponse]} -->     
+                                ${SEI_UOFValList[ITEM_JSONResponse][JSON_ResponseINFOOBJ[ITEM_JSONResponse]]}
+                                <!-- ${SEI_UOFValList[ITEM_JSONResponse]} -->
                             </div>
                         `;
                     }
@@ -125,17 +134,17 @@ function HTTP_Working(HTTP_Response) {
                 }
 
                 let HTML_InfoPart = `
-                    <div class="MF-Item" style="display: flex; align-items: center; justify-content: space-between;">
-                        <div class="MF-ItemHeader"> ${SES_UOFNameList[ITEM_JSONResponse]} </div>
+                    <div class="MI-Item" style="display: flex; align-items: center; justify-content: space-between;">
+                        <div class="MI-ItemHeader"> ${SES_UOFNameList[ITEM_JSONResponse]} </div>
                             ${SES_UOFValUi()}
                     </div>
                 `;
 
-                MF_Content.html(MF_Content.html() + HTML_InfoPart);
+                MI_Content.html(MI_Content.html() + HTML_InfoPart);
             } else if (ITEM_JSONResponse in SES_UOFNameList && SYS_HTTPRequestCount === 1) {
                 let HTML_InfoPartSwipeValue;
 
-                function SYS_MIContent() {
+                function SYS_MMContentCheck() {
                     if (JSON_ResponseINFOOBJ[ITEM_JSONResponse] === 1) {
                         return "checked";
                     } else {
@@ -143,25 +152,25 @@ function HTTP_Working(HTTP_Response) {
                     }
                 }
 
-                HTML_InfoPartSwipeValue = SYS_MIContent();
+                HTML_InfoPartSwipeValue = SYS_MMContentCheck();
                 let HTML_InfoPart = `
-                       <div class="MI-Item" style="display: flex; align-items: center; justify-content: space-between;">
-                           <div class="MI-ItemHeader"> ${SES_UOFNameList[ITEM_JSONResponse]} </div>
+                       <div class="MM-Item" style="display: flex; align-items: center; justify-content: space-between;">
+                           <div class="MM-ItemHeader"> ${SES_UOFNameList[ITEM_JSONResponse]} </div>
                            <div class="form-check form-switch">
-                               <input class="form-check-input MII-${ITEM_JSONResponse}" style="height: 3vh; width: 10vw; margin-right: 2vw;" type="checkbox" role="switch" id="flexSwitchCheckDefault" ${HTML_InfoPartSwipeValue}>
+                               <input class="form-check-input MMI-${ITEM_JSONResponse}" style="height: 3vh; width: 10vw; margin-right: 2vw;" type="checkbox" role="switch" id="flexSwitchCheckDefault" ${HTML_InfoPartSwipeValue}>
                            </div>
                        </div>
                    `;
-                MI_Content.html(MI_Content.html() + HTML_InfoPart);
+                MM_Content.html(MM_Content.html() + HTML_InfoPart);
             }
         }
     }
 }
 
-function SYS_MIContent(APP_CookieJson) {
+function SYS_MMContent(APP_CookieJson) {
     for (let ITEM_UOFNameList in SES_UOFNameList){
         if (ITEM_UOFNameList[0] === "m") {
-            if (document.querySelector(`.MII-${ITEM_UOFNameList}`).checked) {
+            if (document.querySelector(`.MMI-${ITEM_UOFNameList}`).checked) {
                 APP_CookieJson[ITEM_UOFNameList] = 1;
             } else {
                 APP_CookieJson[ITEM_UOFNameList] = 0;
@@ -173,41 +182,49 @@ function SYS_MIContent(APP_CookieJson) {
 }
 
 function BM_Show() {
-    anime({
-        targets: '.Header-BurgerMenuIcon',
-        opacity: ['1', '0'],
-        duration: 500,
-        loop: false
-    });
+    if (!BMC_status) {
+        anime({
+            targets: '.Header-BurgerMenuIcon',
+            opacity: ['1', '0'],
+            duration: 500,
+            loop: false
+        });
 
-    anime({
-        targets: ['.BM-Content', '.BM-BlackScreen'],
-        translateX: ['-150%', '0'],
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
+        anime({
+            targets: ['.BM-Content', '.BM-BlackScreen'],
+            translateX: ['-150%', '0'],
+            easing: 'easeInOutQuad',
+            direction: 'alternate',
+            duration: 1000,
+            loop: false
+        });
+
+        BMC_status = 1
+    }
 }
 
 function BM_Hide() {
-    anime({
-        targets: ['.BM-Content', '.BM-BlackScreen'],
-        translateX: ['0', '-150%'],
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        duration: 1000,
-        loop: false
-    });
-
-    setTimeout(() => {
+    if (BMC_status) {
         anime({
-            targets: '.Header-BurgerMenuIcon',
-            opacity: ['0', '1'],
-            duration: 500,
+            targets: ['.BM-Content', '.BM-BlackScreen'],
+            translateX: ['0', '-150%'],
+            easing: 'easeInOutQuad',
+            direction: 'alternate',
+            duration: 1000,
             loop: false
-        })
-    }, 1000);
+        });
+
+        setTimeout(() => {
+            anime({
+                targets: '.Header-BurgerMenuIcon',
+                opacity: ['0', '1'],
+                duration: 500,
+                loop: false
+            })
+        }, 1000);
+
+        BMC_status = 0
+    }
 }
 
 HTTP_Request()
